404314eab50beacb67b6ee6438cbc337f44e549d
Brian O'Connor <brian@btoconnor.net>
2013-12-25 18:58:10 -0500

    Adding simplejson as a project dependency

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
         "unstdlib",
         "turbomail",
         "pytz",
+        "simplejson",
     ],
     setup_requires=["PasteScript>=1.6.3"],
     packages=find_packages(exclude=['ez_setup']),
